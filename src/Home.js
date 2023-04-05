6637961f57319127637f951de5d53980e8fcbbac
abi-manoharan97 <amanoh15@my.centennialcollege.ca>
2023-04-05 17:12:46 -0400

    finalized home

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,8 @@ function Home(props) {
             <div class="names">Lewis Merino - <strong>822273553</strong></div>
             <div class="names">Vinit Purohit - <strong>301165253</strong></div>
             
-            <br/><br/>
+            <br/><br/> 
+            
             <div id="description">
                 This application predicts the writing test score of a student using a model trained on the following features: gender, race/ethnicity, parental level of education, lunch, math score, reading score, test preparation course. Click the "Predict" button on the far right to begin.
             </div> 
